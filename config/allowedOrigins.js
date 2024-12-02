23cc4c0dc72ce140842a5c416317ee48944d3e40
Gnjurats <brandstattern@gmail.com>
2024-12-02 19:44:12 +0100

    Commit changes

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,9 @@
 const allowedOrigins = [
+    'http://localhost:3000',
     'https://www.gag-api.onrender.com',
+    'https://www.gag.onrender.com',
+    'https://technotes.onrender.com'
+    
 ]
 
 module.exports = allowedOrigins
