861688116b534756dcd96ecad8ad5d020a43fa0a
Gnjurats <brandstattern@gmail.com>
2024-12-02 19:52:58 +0100

    Commit changes

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,8 @@ const allowedOrigins = [
     'http://localhost:3000',
     'https://www.gag-api.onrender.com',
     'https://www.gag.onrender.com',
-    'https://technotes.onrender.com'
+    'https://technotes.onrender.com',
+    'https://technotes-api.onrender.com'
     
 ]
 
